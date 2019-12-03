d1ab884c84ac08f4ea13a0a4aeefd83689591dca
David <david_geran@live.de>
2019-12-03 05:54:21 +0100

    working on join page, created box for join

--- whereismyrave/src/components/Join.jsx
+++ whereismyrave/src/components/Join.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import ReactCodeInput from 'react-code-input'
 
 class Join extends React.Component {
 	render() {
-		return <p>Join Test</p>
+		return (
+			<form onSubmit={e => this.join}>
+				<div class="roundedCorners">
+					<label>E-Mail</label>
+					<input type="email"></input>
+					<button>Invite</button>
+				</div>
+				<div>
+					<label>Code</label>
+					<ReactCodeInput type="password" fields={4} />
+				</div>
+			</form>
+		)
 	}
 }
 
